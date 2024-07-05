9d1d958487b3ffcb393c7d675e4966f05a3ff6f1
Administrator <mj17428@naver.com>
2024-07-05 14:48:33 +0900

    [김민지] - owner glamping 포스트 작업 중

--- src/main/java/com/green/glampick/mapper/OwnerMapper.java
+++ src/main/java/com/green/glampick/mapper/OwnerMapper.java
@@ -1,10 +1,15 @@
 package com.green.glampick.mapper;
 
+import com.green.glampick.dto.object.glamping.RoomItem;
+import com.green.glampick.dto.request.GlampingPostRequestDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OwnerMapper {
     // 민지
-
+    void insertGlamping(GlampingPostRequestDto req);
+    void insertRoom(RoomItem req);
+    void insertRoomImg (RoomItem req);
+    void insertRoomService(RoomItem req);
     // 강국
 }
